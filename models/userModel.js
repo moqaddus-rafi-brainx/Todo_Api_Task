ffc8f9872d30966329227774545b2cd5a79843c2
moqaddus-rafi-brainx <moqaddus.rafi@brainxtech.com>
2025-05-05 14:16:11 +0500

    Updated userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -6,11 +6,42 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim:true,
   },
-  token: {
-    type: String,
-    required: false,
-  },
+  email:{
+    type:String,
+    trim:true,
+  }
 });
+
+//EMAIL VALIDATION
+userSchema.pre('save', async function (next) {
+  const User = mongoose.model('User');
+  //return if its not modified(or added)
+  if(!this.isModified('email'))
+  {
+    return next();
+  }
+  //Checking that email is not empty or just spaces
+  if (this.email==undefined || this.email.trim() === '') {
+    return next(new Error('Email cannot be empty'));
+  }
+
+  //Checking email syntax
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(this.email)) {
+    return next(new Error('Invalid email format'));
+  }
+
+  //Checking if email already exists
+  const existingUser = await User.findOne({ email: this.email });
+  if (existingUser) {
+    const err = new Error('Email already exists');
+    err.status = 400;
+    return next(err);
+  }
+  next();
+});
+
+
 
 //NAME VALIDATION
 userSchema.pre('save', async function (next) {
