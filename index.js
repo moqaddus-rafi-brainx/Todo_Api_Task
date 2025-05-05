85eab749e1b134541f6dc51e44795c2fbdc1a2b7
moqaddus-rafi-brainx <moqaddus.rafi@brainxtech.com>
2025-05-05 15:54:13 +0500

    routes corrected, db.js added

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config(); 
 const userRoutes=require('./routes/userRoutes');
 const taskRoutes=require('./routes/taskRoutes');
+const connectDB=require('./config/db');
 const app = express();
 
 app.use(express.json());
-app.use('/api', userRoutes);
-app.use('/api/task',taskRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/tasks',taskRoutes);
 const port=process.env.PORT;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('✅ Connected to MongoDB via Mongoose'))
-.catch(err => console.error('❌ MongoDB connection error:', err));
-
+//Connect to MongoDB
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server on http://localhost:${process.env.port}`);
